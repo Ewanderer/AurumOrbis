bbab88e85e33d38179a019c66264f8f45e98091f
Ewanderer <jordan.eichner@gmx.de>
2015-04-14 15:36:31 +0200

    Dice-Roll 2
    Generelle Überarbeitung, weil fehlerhaft

--- TopDownRPG/Assets/StandardAssets/RPG-Sources/Scripts/Meta/Dice.cs
+++ TopDownRPG/Assets/StandardAssets/RPG-Sources/Scripts/Meta/Dice.cs
@@ -10,43 +10,80 @@ public static class Dice {
 	/**
 	 * \brief Diese Funktion macht einen regülaren Check gegen eine beliebiege Anzahl an Limits
 	 * 
-	 * \param Pool In:Pool an Punkten die Ausgeglichen werden müssen oder anders herum. Out: Anzahl der Punkte die Übrig sind oder die nicht ausgeglichen werden konnten
+	 * \param Pool Punkte zum Ausgleichen oder bei negativen Werten, die ausgeglichen werden
 	 * \param IsCritical Gibt an ob es sich um ein kritisches Ergebnis handelt. Anhand von Result erkennt man ob positiv oder negativ.
 	 * \param Limits Die Einzelnen Limits, gegen die man würfelt. Differenzen werden mit dem Pool verechnet
 	 * \return Auskunft ob der Check gelungen ist
 	 * 
 	 * 
 	 */
-	public static bool RollCheck(ref int Pool, out bool IsCritical, params int[] Limits){
-		int bonus = 0;
+	public static bool RollCheck(int Pool, out bool IsCritical, params int[] Limits){
 		bool failed = false;
 		int criticalcounter = 0;//Wert der die critical result zählt.
 		foreach (int l in Limits) {
 			int r=Random.Range(1,21);
 			if(r==1)
 				criticalcounter++;
+			else
 			if(r==20)
 				criticalcounter--;
-			if(r>l){
+			else{
+			if(r>l)
 				if(Pool>=r-l)
 					Pool-=r-l;
-			else{
+					else
+						failed=true;
+			else
 					failed=true;
-				}
-			}else{
-				if(Pool>0)
-				bonus+=l-r;
+				
 			}
 			}
 		IsCritical = criticalcounter != 0;
-		if (Pool > 0)
-			Pool += bonus;
 		if (criticalcounter < 0 || failed)
 			return false;
 		else
 			return true;
 	}
 		
+	public static bool RollCheck(int SituationPool, ref int Bonus,out bool IsCritical, params int[] Limits){
+		int Pool = SituationPool + Bonus;
+		int criticalcounter = 0;
+		bool failed = false;
+		int overall = 0;
+		foreach (int l in Limits) {
+			int r=Random.Range(1,21);
+			if(r==1)
+				criticalcounter++;
+			else
+				if(r==20)
+					criticalcounter--;
+			else{
+				if(l<r){
+					//Prüfen ob Probe aufgeglichen werden kann
+					if(Pool>r-l)
+						Pool-=r-l;
+					else
+						failed=true;
+				}else{
+					overall+=l-r;
+				}
+			}
+		}
+		IsCritical = criticalcounter != 0;
+			Pool += overall;
+		if (Pool > Bonus)
+			Pool = Bonus;
+		if (Pool > 0)
+			Bonus = Pool;
+		else
+			Bonus = 0;
+		if (failed || criticalcounter < 0)
+			return false;
+		else
+			return true;
+
+
+	}
 
 
 }
