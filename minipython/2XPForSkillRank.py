a957299f6237d5b92832709e379c35f55463e5af
Ewanderer <jordan.eichner@gmx.de>
2015-11-21 10:24:03 +0100

    Krams ist toll

--- minipython/2XPForSkillRank.py
+++ minipython/2XPForSkillRank.py
@@ -1,9 +1,13 @@
+import math;
 mod=float(input("Lernmodifikator:"));
 rank=float(input("Rangziel:"));
+c=pow(10,0.5);
 xp=0.0;
 while True:
 	xp+=1;
-	if (1/9)*(-140+2*pow(10,0.5)*pow(9*xp*mod+490,0.5))>=rank:
+	a=pow((9*xp*mod)+490 ,0.5)
+	if (1/9)*(-140+2*c*a) >= rank:
 		break;
 print("Benötigte XP:"+str(xp));
-	
+
+print(math.ceil(((7*rank)+(9*rank*(rank/40)))*pow(mod,-1) ) );	
